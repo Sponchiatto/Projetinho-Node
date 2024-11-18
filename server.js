dfea580f9d506bb403175e0bfff82ea60298f943
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-11-18 20:24:54 -0300

    Iniciando em Node e Express

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000, seu coco");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hihihihi.");
+});
