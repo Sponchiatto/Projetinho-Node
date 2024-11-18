c1783215e5c25a840dcd3632b889d2af754dfc8e
Sponchiatto <rafael_sponchiatto@hotmail.com>
2024-11-18 20:25:45 -0300

    NFC

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000, seu coco");
+  console.log("Server is running on port 3000");
 });
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hihihihi.");
+  res.status(200).send("É isso");
 });
